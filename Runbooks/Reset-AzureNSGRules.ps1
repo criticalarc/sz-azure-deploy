571928ede98ad23efd53fed095acb2b13250c596
Bevan Goddard <bevan@criticalarc.com>
2024-01-19 09:28:07 +1000

    bg/ITO-127 Reset-Nsg runbook improvement

--- Runbooks/Reset-AzureNSGRules.ps1
+++ Runbooks/Reset-AzureNSGRules.ps1
@@ -21,23 +21,55 @@ if (!($AzureContext.Subscription)) {
 }
 
 $SubscriptionId = Get-AutomationVariable -Name 'SubscriptionId'
+$deploymentCode = Get-AutomationVariable -Name 'DeploymentCode'
+$locationCode = Get-AutomationVariable -Name 'LocationCode'
+
 $AzureContext = Set-AzContext -SubscriptionId "$SubscriptionId" -DefaultProfile $AzureContext
 
-$NsgReferences = Get-AzResource -Name "*-reference" -ResourceType Microsoft.Network/networkSecurityGroups
+$NetworkNumber = switch ($DeploymentCode) {
+    "d" { "0" }
+    "t" { "10" }
+    "p" {
+        switch ($LocationCode) {
+            "ae" { "20" }
+            "uks" { "30" }
+            "wus" { "40" }
+        }
+    }
+}
+
+$NsgReferences = Get-AzResource -Name "*-$deploymentCode-*-reference" -ResourceType Microsoft.Network/networkSecurityGroups
 
 if ($NsgReferences) {
     foreach ($NsgReference in $NsgReferences) {
         $NsgReference = $NsgReference | Get-AzNetworkSecurityGroup
 
         $NsgTargetName = [regex]::Matches($NsgReference.Name, "(^\w.+)(-reference)").Groups[1].Value
+        $NsgTargetName = $NsgTargetName -replace ("-ae-","-$locationCode-")
         $NsgTarget = Get-AzNetworkSecurityGroup -Name $NsgTargetName
 
         if ($NsgTarget) {
-            $NsgTarget.SecurityRules = $NsgReference.SecurityRules
+            $NsgRules=@()
 
-            Write-Host "Resetting security rules in '$($NsgTarget.Name)' using '$($NsgReference.Name)' as reference."
-        
+            $NsgReference.SecurityRules | ForEach-Object {
+                $NsgRules += New-AzNetworkSecurityRuleConfig -Name $_.Name `
+                    -Protocol $_.Protocol `
+                    -SourcePortRange $_.SourcePortRange `
+                    -DestinationPortRange $_.DestinationPortRange `
+                    -SourceAddressPrefix $_.SourceAddressPrefix `
+                    -DestinationAddressPrefix ($_.DestinationAddressPrefix -replace ("20","$NetworkNumber")) `
+                    -SourceApplicationSecurityGroup $_.SourceApplicationSecurityGroup `
+                    -DestinationApplicationSecurityGroup $_.DestinationApplicationSecurityGroup `
+                    -Access $_.Access `
+                    -Priority $_.Priority `
+                    -Direction $_.Direction
+            }
+
+            $NsgTarget.SecurityRules = $NsgRules
+
+            Write-Output "Resetting security rules in '$($NsgTarget.Name)' using '$($NsgReference.Name)' as reference."
             $NsgTarget | Set-AzNetworkSecurityGroup | Select-Object -ExpandProperty SecurityRules | Format-Table
+
         } else {
             Write-Output "NSG '$NsgTargetName' not found in Azure subscription '$($AzureContext.Subscription.Name)'. Security rules from NSG '$($NsgReference.Name)' not applied."
         }
